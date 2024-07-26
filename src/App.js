a47c0ad9d7d04120827ab9eb5fbeafd9f3e038d5
kscarby <sabrina.keito@gmail.com>
2024-07-26 11:14:53 -0300

    modificaçes no app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import Toolbar from './components/Toolbar';
 import Body from './components/Body';
 import ProductCard from './components/ProductCard';
 import PrimarySearchAppBar from  './components/PrimarySearchAppBar';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme} from '@mui/material/styles';
 
 function App() {
+
+  // paleta de cores
+  const theme = createTheme({
+    palette: {
+      sage: {
+        main: '#B5C18E',
+      },
+      beige: {
+        main: '#F7DCB9',
+      },
+      tamara: {
+        main: '#DEAC80',
+      },
+      castain: {
+        main: '#B99470',
+      },
+    },
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <Toolbar />
+        <Toolbar theme = {theme}/>
       </header>
       <body className='App-body'>
         <div className='Banner-body'>
